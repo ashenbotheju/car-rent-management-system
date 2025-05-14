139af3876db411e293ca6e71d8f058302017a9b2
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2025-05-15 02:23:24 +0530

    contact form update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,7 +13,7 @@
         <div class="grid md:grid-cols-2 gap-12">
             <!-- Contact Form -->
             <div class="md:col-span-1">
-                <form action="#" class="space-y-6">
+                {{-- <form action="#" class="space-y-6">
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                         <input type="email" id="email" class="block p-3 w-full text-sm rounded-lg border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="name@example.com" required>
@@ -28,6 +28,34 @@
                     </div>
                     <button type="submit" class="w-full md:w-auto px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
                         Send message
+                    </button>
+                </form> --}}
+                
+                <form action="https://api.web3forms.com/submit" method="POST">
+                    <!-- Replace with your Access Key -->
+                    <input type="hidden" name="access_key" value="6fb88fad-db06-4b08-9f3e-d45e53be2396">
+
+                    <!-- Form Inputs. Each input must have a name="" attribute -->
+                    <div>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+                        <input type="text" name="name" class="block mb-2 p-3 w-full text-sm rounded-lg border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Ashen botheju" required>
+                    </div>
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                        <input type="email" name="email" class="block mb-2 p-3 w-full text-sm rounded-lg border border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="ashen@example.com" required>
+                    </div>
+                    <div>
+                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Message</label>
+                        <textarea name="message" rows="5" class="block mb-5 p-2.5 w-full text-sm rounded-lg shadow-sm border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Your message....." required></textarea>
+                    </div>
+                    <!-- Honeypot Spam Protection -->
+                    <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
+
+                    <!-- Custom Confirmation / Success Page -->
+                    <!-- <input type="hidden" name="redirect" value="https://mywebsite.com/thanks.html"> -->
+
+                    <button type="submit" class="w-full md:w-auto px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
+                        Submit Form
                     </button>
                 </form>
             </div>
