ba2d2eeef6de5dd88d8724d1c33c0ff3a871f6b2
yohan vishvajith <yohanvishvajith@gmail.com>
2025-07-11 23:25:31 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\StripePaymentController;
 
-use Laravel\Socialite\Facades\Socialite;
+
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/login', [HomeController::class, 'login'])->name('login');
